b2a640b014a416a6a6a4d4e011280ab8c6fbb533
Mufaqar <mufaqar@gmail.com>
2026-05-21 18:03:56 +0500

    robots

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-    const baseUrl = 'https://frontend-hajjumrah.holyvibes.co.uk';
+    const baseUrl = 'https://umrahholidays.co.uk/';
 
     return {
         rules: [
