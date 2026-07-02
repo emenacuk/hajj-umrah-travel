1886757ed5ad4ae112f6351438d37b0ce553709e
Mufaqar <mufaqar@gmail.com>
2026-07-02 13:04:56 +0500

    bs url

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-    const baseUrl = 'https://umrahholidays.co.uk/';
+    const baseUrl = 'https://umrahholidays.co.uk';
 
     return {
         rules: [
